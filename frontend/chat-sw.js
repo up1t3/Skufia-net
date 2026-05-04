350e3209983bb5b758e35194668978cdb1634d83
Up1t3 <up1t3@urbantaxi.com>
2026-05-04 19:10:23 +0300

    chore(release): v2.2.0_04.05_16:10

--- frontend/chat-sw.js
+++ frontend/chat-sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'skufia-chat-v2.2.0_04.05_15:55'; // Bumped to force update of chat_core.js and password reset UI
+const CACHE_NAME = 'skufia-chat-v2.2.0_04.05_16:10'; // Bumped to force update of chat_core.js and password reset UI
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
