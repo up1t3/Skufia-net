1231935fe23782856b44b543ed830b2ed2e6a741
Up1t3 <up1t3@urbantaxi.com>
2026-05-04 12:28:53 +0300

    chore: Auto-commit before deploy

--- check_remote_users.js
+++ check_remote_users.js
@@ -3,7 +3,7 @@ const { Client } = require('ssh2');
 
 const conn = new Client();
 conn.on('ready', () => {
-    conn.exec(`which socat`, (err, stream) => {
+    conn.exec(`docker exec skufia-smtp-proxy socat -V`, (err, stream) => {
         if (err) throw err;
         stream.on('close', (code, signal) => {
             conn.end();
